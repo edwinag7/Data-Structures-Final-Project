d5ec44a9f7d2dd38787e52c6f2dd93d429449b11
edwinag7 <edwin.guerra@colorado.edu>
2016-04-26 20:36:17 -0600

    Added files via upload

--- Planner.cpp
+++ Planner.cpp
@@ -2,16 +2,23 @@
 
 Planner::Planner()
 {
-    head=NULL;
+    head = NULL;
 }
 
 Planner::~Planner()
 {
     //dtor
 }
+void Planner::clearPlanner(){
+     Plans *tmp=head;
+     while(tmp!=NULL){
+         head=head->next;
+         delete head;
+         tmp=head;
+     }
 
+}
 void Planner::printEvent(){
-
     Plans *tmp=NULL;
     tmp=head;
     while(tmp!=NULL){
@@ -20,25 +27,40 @@ void Planner::printEvent(){
         cout<<"Event "<<tmp->Event<<endl;
         tmp=tmp->next;
     }
-
 }
-
 void Planner::addEvent(string time, string planname, string planvalue)
 {
         if(head==NULL){
             Plans *test=new Plans(time, planname, planvalue, NULL,NULL);
             head=test;
         }
-
         else{
         Plans *current = head;
         while(current->next!=NULL){
             current=current->next;
         }
-
         Plans *addPlan=new Plans(time, planname, planvalue, NULL, NULL);
         addPlan->next=current->next;
         addPlan->previous=current;
         current->next=addPlan;
         }
+}
+void Planner::deleteEvent(string time, string planname, string planvalue){
+
+
+
+
+        Plans *current = head;
+        Plans *Prev = NULL;
+        while(current != NULL &&(current->Event != time)&&(current->Time != planname)&&(current->Values != planvalue)){
+            current=current->next;
+        }
+        if(current != NULL){
+
+
+        }
+        else{
+            cout << "not found" << endl;
+        }
+
 }
